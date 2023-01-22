8b096f7fdcb136c92168fc90a7e681e489e2a557
llorenc-fer <119510686+llorenc-fer@users.noreply.github.com>
2023-01-22 19:59:37 +0100

    Update appsd.py

--- appsd.py
+++ appsd.py
@@ -1,7 +1,6 @@
 #-----librerías----------------------------------------------------------------------------------------
 import pandas as pd
 import numpy as np
-import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.image import imread
 import plotly.express as px
